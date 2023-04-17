b0035f51bb02810d02ab622194dd036f3d725bb0
karnika gupta <karnikagupta1830@gmail.com>
2023-04-17 18:10:50 +0530

    complete build

---
+++ src/components/Banner.jsx
@@ -0,0 +1,42 @@
+import React, { useState, useEffect } from "react";
+import Weather from "./Weather";
+
+const Banner = () => {
+
+    const [lat, setLat] = useState([]);
+    const [long, setLong] = useState([]);
+    const [data, setData] = useState([]);
+
+    useEffect(() => {
+        const fetchData = async () => {
+
+          //navigator.geolocation is js api which takes permission from the user to access the location
+          navigator.geolocation.getCurrentPosition(function(position) {
+            setLat(position.coords.latitude);
+            setLong(position.coords.longitude);
+          });
+
+          await fetch(`https://api.openweathermap.org/data/2.5/weather?lat=${lat}&lon=${long}&units=metric&&appid=${process.env.REACT_APP_API_KEY}`)
+          .then(res => res.json())
+          .then(result => {
+              setData(result)
+              console.log(result);
+            });
+        } 
+        fetchData();   
+    }, [lat, long]);
+
+    return (
+        <div className="flex justify-center">
+            <div className=" backdrop-blur-md shadow-2xl shadow-slate-500 bg-opacity-80 self-center p-16 lg:mt-56 ml-64 mr-64 grow rounded-md">
+                {(typeof data.main !== 'undefined') ? (
+                    <Weather weatherData={data} />
+                ) : (
+                    <div></div>
+                )}
+            </div>
+        </div>
+    )
+}
+
+export default Banner;
---
+++ src/components/Weather.jsx
@@ -0,0 +1,55 @@
+import { ImLocation2 } from 'react-icons/im'
+import { HiCalendarDays } from 'react-icons/hi2'
+import { AiOutlineClockCircle } from 'react-icons/ai'
+import { BsCalendarDate, BsSun, BsSunrise, BsFillCloudsFill, BsSunset} from 'react-icons/bs'
+import { BiRefresh } from 'react-icons/bi'
+import moment from 'moment';
+
+const refresh = () => {
+    window.location.reload();
+}
+
+const Weather = ({ weatherData }) => {
+    return (
+        <div className="text-white font-thin">
+            <div className="lg:text-5xl text-3xl flex justify-center">
+                {weatherData.main.temp}&deg;C
+                <button onClick={refresh}>
+                    <BiRefresh size={25} className='lg:ml-3 ml-8'/>
+                </button>
+            </div>
+            <div className="flex justify-center mt-4 text-xl lg:text-3xl">
+                <ImLocation2 className='mt-1 mr-2' size={30}/>
+                {weatherData.name}
+            </div>
+            <div className='lg:grid lg:grid-cols-3 lg:pl-28 lg:gap-8 lg:text-3xl text-xl mt-20 '>
+                <div className='flex'> 
+                    <HiCalendarDays className='mr-2 mt-1'/>
+                    {moment().format('dddd')}
+                </div>
+                <div className='flex'>
+                    <AiOutlineClockCircle className='mt-1 mr-2'/>
+                    {moment().format('LT')}</div>
+                <div className='flex'>
+                    <BsCalendarDate className='mr-3 mt-1'/>
+                    {moment().format('LL')}
+                </div>
+                <div className='flex'>
+                    {(weatherData.weather[0].main === "Clear") ? <BsSun className='mt-1 mr-2'/> : <BsFillCloudsFill />}
+                    {weatherData.weather[0].main}
+                </div>
+                <div className='flex'>
+                    <BsSunrise className='mr-2 mt-1'/>
+                    {new Date(weatherData.sys.sunrise * 1000).toLocaleTimeString('en-IN')}
+                </div>
+                <div className='flex'>
+                    <BsSunset className='mr-2 mt-1'/>
+                    {new Date(weatherData.sys.sunset * 1000).toLocaleTimeString('en-IN')}
+                </div>
+
+            </div>
+        </div>
+    )
+}
+
+export default Weather;
